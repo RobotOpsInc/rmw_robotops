69ebbfcf40b2041b7942ea1c0de8c54a2f2cb676
Kristoph Matthews <krist.m@gmail.com>
2026-01-04 21:55:35 -0500

    fix: Add dds_metadata.cpp stub
    Last missing source file needed for build.
    
    Related to ROB-55

---
+++ src/dds_metadata.cpp
@@ -0,0 +1,4 @@
+// Copyright 2025 Robot Ops Inc.
+// Licensed under the Apache License, Version 2.0
+// DDS metadata injection/extraction - stub for now
+// TODO: Implement context injection/extraction via DDS property lists
